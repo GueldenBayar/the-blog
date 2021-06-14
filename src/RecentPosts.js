93e942f8639c251ed8d0f7bfeb959358b458212f
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-06-14 17:53:59 +0200

    dynamic fetch implemented working including .env

--- src/RecentPosts.js
+++ src/RecentPosts.js
@@ -3,25 +3,28 @@ import Map from "./Map";
 import SummarizedPost from "./SummarizedPost";
 import { Link } from "react-router-dom";
 
-
-export default function RecentPosts({posts}) {
-    console.log(posts)
-    return (
-       <> 
-       <section className="col-5">
+export default function RecentPosts({ posts }) {
+  //console.log(posts)
+  return (
+    <>
+      <section className="col-5">
         <ul>
-         {posts.map(post => {
-             return ( 
-                 <li><Link to={`/post/${post.sys.id}`}>Link to post with id: {post.sys.id}</Link></li>
-             )
-         })}
+          {posts.map((post) => {
+            return (
+              <li>
+                <Link to={`/post/${post.sys.id}`}>
+                  Link to post with id: {post.sys.id}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-            {/* // Content: e.g. list of four most recent entries in database */}
-            {/* <SummarizedPost/> */}
-        </section>
-        <section className="col-7">
-            <Map></Map>
-        </section>
-       </> 
-    );
+        {/* // Content: e.g. list of four most recent entries in database */}
+        {/* <SummarizedPost/> */}
+      </section>
+      <section className="col-7">
+        <Map></Map>
+      </section>
+    </>
+  );
 }
