7a7d2206139c2dbb0585f0256ea996c3426b2075
Franz <franz.hanschmidt@posteo.de>
2021-06-16 17:17:40 +0200

    fix styling CardPost

--- src/CardPost.js
+++ src/CardPost.js
@@ -10,64 +10,30 @@ function CardPost(props) {
   return (
     <div className="cardPost">
       <div>
-        <p className="title">
-          <Link
-            to={`/post/${props.ids}`}
-            style={{
-              textDecoration: "none",
-              color: "white",
-              fontStyle: "bold",
-              fontWeight: "bold",
-              fontStyle: "italic",
-              fontSize: "x-large",
-            }}
-          >
-            {props.title}
-          </Link>
-        </p>
+        <p className="title">{props.title}</p>
         <div style={{ minHeight: "50px" }}>
           <p className="desc">
             <Link
               to={`/post/${props.ids}`}
-              style={{
-                textDecoration: "none",
-                color: "#053E04",
-                fontWeight: "bold",
-              }}
+              style={{ textDecoration: "none", color: "black" }}
             >
               {subS1} ...
             </Link>
           </p>
         </div>
         <div clasName="bottom row">
-          <div className="column1">
-            <p
-              className="info"
-              style={{ textDecoration: "none", color: "#053E04" }}
-            >
-              <b>You'll find the tennis-table here:</b> {props.adress}
-            </p>
+          <div className="column">
+            <p className="info">{props.adress}</p>
           </div>
-          <div className="column2">
-            <p
-              className="info"
-              style={{ textDecoration: "none", color: "#053E04" }}
-            >
-              <b>Delicious food and lots to drink:</b> {props.supplies}
-            </p>
+          <div className="column">
+            <p className="info">{props.supplies}</p>
           </div>
-          <div className="column3">
+          <div className="column">
             <img
               className="circle-img"
               src="https://www.liveabout.com/thmb/wvVhsu7tXYrZV664uVEPSc0b-44=/1413x1413/smart/filters:no_upscale()/114996552-57bb0f545f9b58cdfde2205e.jpg"
             />
           </div>
-        </div>
-        <div className="column">
-          <img
-            className="circle-img"
-            src="https://www.liveabout.com/thmb/wvVhsu7tXYrZV664uVEPSc0b-44=/1413x1413/smart/filters:no_upscale()/114996552-57bb0f545f9b58cdfde2205e.jpg"
-          />
         </div>
       </div>
     </div>
