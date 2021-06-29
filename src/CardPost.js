c6886e91c0361d3c8ac190be227ff412623c25c7
Franz <franz.hanschmidt@posteo.de>
2021-06-29 16:04:27 +0200

    fix css CardPost, not done

--- src/CardPost.js
+++ src/CardPost.js
@@ -3,36 +3,41 @@ import "./CardPost.css";
 import { Link } from "react-router-dom";
 
 function CardPost(props) {
-  const [highlightStyle, setHighlightStyle] = useState(`cardPost`);
-  const handleMouseOver = (e) => {
-    highlightStyle === `cardPost highlight`
-      ? setHighlightStyle(`cardPost`)
-      : setHighlightStyle(`cardPost highlight`);
-  };
+  // const [highlightStyle, setHighlightStyle] = useState(`cardPost`);
+  // const handleMouseOver = (e) => {
+  //   highlightStyle === `cardPost highlight`
+  //     ? setHighlightStyle(`cardPost`)
+  //     : setHighlightStyle(`cardPost highlight`);
+  // };
 
-  const subS = props.desc;
-  let subS1;
-  if (!subS) {
-    subS1 = "";
-  } else {
-    subS.length >= 200
-      ? (subS1 = subS.substr(0, 200) + `  ...`)
-      : (subS1 = subS.substr(0, subS.length));
-    console.log(subS);
-  }
+  // const subS = props.desc;
+  // let subS1;
+  // if (!subS) {
+  //   subS1 = "";
+  // } else {
+  //   subS.length >= 200
+  //     ? (subS1 = subS.substr(0, 200) + `  ...`)
+  //     : (subS1 = subS.substr(0, subS.length));
+  //   console.log(subS);
+  // }
 
   return (
-    <div className={highlightStyle}>
+    <div className="cardPost">
       <Link
-        onMouseEnter={handleMouseOver}
-        onMouseLeave={handleMouseOver}
         to={`/post/${props.ids}`}
         style={{ textDecoration: "none", color: "black" }}
       >
+        <div className="p-3">
+          <img
+            // className="circle-img"
+            src="https://www.liveabout.com/thmb/wvVhsu7tXYrZV664uVEPSc0b-44=/1413x1413/smart/filters:no_upscale()/114996552-57bb0f545f9b58cdfde2205e.jpg"
+            alt="logo"
+          />
+        </div>
         <div className="p-2">
           <h2 className="fs-3 text-break mb-3">{props.title}</h2>
 
-          <p className="">{subS1}</p>
+          <p className="">{props.description}</p>
 
           <div className="bottom row">
             <div className="column">
@@ -44,14 +49,6 @@ function CardPost(props) {
                 {props.supplies}
               </p>
             )}
-
-            <div className="p-3">
-              <img
-                className="circle-img"
-                src="https://www.liveabout.com/thmb/wvVhsu7tXYrZV664uVEPSc0b-44=/1413x1413/smart/filters:no_upscale()/114996552-57bb0f545f9b58cdfde2205e.jpg"
-                alt="logo"
-              />
-            </div>
           </div>
         </div>
       </Link>
--- src/RecentPosts.js
+++ src/RecentPosts.js
@@ -17,37 +17,33 @@ export default function RecentPosts({ posts }) {
   return (
     <>
       <section className="col-5 content-container-custom px-4 mt-5 ms-5 me-3">
-        <ul>
-          {/* maps over the post, compares the posts update date with 
+        {/* maps over the post, compares the posts update date with 
                     all dates in the array cPosts, if later add a 1 to the place
                     if place is lower 5 (1-4 newesrt entries), cardPost is returned */}
-          {!posts.length && (
-            <h6>Sorry, your search did not match any results</h6>
-          )}
-          {posts.length &&
-            posts.map((post) => {
-              // let d1 = post.updatedAt;
-              // let place = 1;
-              // cPosts.map((cPost) => {
-              //   if (d1 > cPost) {
-              //     place++;
-              //   }
-              // });
+        {!posts.length && <h6>Sorry, your search did not match any results</h6>}
+        {posts.length &&
+          posts.map((post) => {
+            // let d1 = post.updatedAt;
+            // let place = 1;
+            // cPosts.map((cPost) => {
+            //   if (d1 > cPost) {
+            //     place++;
+            //   }
+            // });
 
-              // if (place < 20)
-              return (
-                <>
-                  <CardPost
-                    desc={post.description}
-                    title={post.title}
-                    adress={post.adress}
-                    supplies={post.supplies}
-                    ids={post.id}
-                  />
-                </>
-              );
-            })}
-        </ul>
+            // if (place < 20)
+            return (
+              <>
+                <CardPost
+                  desc={post.description}
+                  title={post.title}
+                  adress={post.adress}
+                  supplies={post.supplies}
+                  ids={post.id}
+                />
+              </>
+            );
+          })}
         {/* // Content: e.g. list of four most recent entries in database */}
         {/* <SummarizedPost/> */}
       </section>
